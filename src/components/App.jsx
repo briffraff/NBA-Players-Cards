4c9ab520dd6de261f6a6f60385d97b9ea7aef91f
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-01 02:28:10 +0300

    Initialize the project code (#1)

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './User/Login/Login'
+
+export default function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
