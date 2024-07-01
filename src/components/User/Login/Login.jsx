cd7c69e59cc5b3175df964389d71139decfaa404
Borislav Borisov <briffraff6@gmail.com>
2024-07-01 04:50:30 +0300

    Add Login component
    Add login component and beautify it with scss

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -1,26 +1,21 @@
 export default function Login() {
     return (
         <>
-            <main class="site-content">
-                <div class="site-wrapper">
-                    <div class="login-box">
+            <div className="login-box">
 
-                        <h1 class="info-slogan">Login</h1>
-                        <div class="textbox">
-                            <i class="fas fa-user"></i>
-                            <input type="text" placeholder="Username" />
-                        </div>
-
-                        <div class="textbox">
-                            <i class="fas fa-lock"></i>
-                            <input type="text" placeholder="Password" />
-                        </div>
-
-                        <input class="login-btn" type="button" value="Log in" />
-                    </div>
+                <h1 className="info-slogan">Login</h1>
+                <div className="textbox">
+                    <i className="fas fa-user"></i>
+                    <input type="text" placeholder="Username" />
                 </div>
 
-            </main>
+                <div className="textbox">
+                    <i className="fas fa-lock"></i>
+                    <input type="text" placeholder="Password" />
+                </div>
+
+                <input className="login-btn" type="button" value="Log in" />
+            </div>
         </>
     )
 }
