b116beba518e839b5d7320a3e5daf8e9d97fedca
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-05 20:00:31 +0300

    transfer auth functions to external service file (#6)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,22 @@
-// import defaults
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//import styles
-import '../../public/assets/css/App.css'
+import "../../public/assets/css/App.css";
 
-//import components
-import Login from './User/Login/Login'
-import Register from './User/Register/Register'
-import Home from './Home/Home'
+import Home from "./Home/Home";
+import Login from "./User/Login/Login";
+import Register from "./User/Register/Register";
+import Logout from "./User/Logout/Logout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+        <Route index path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { logoutUser } from "../../service/authentication/authService";
 
 export default function Header() {
+    const handleLogout = async () => {
+        try {
+            await logoutUser();
+        } catch (error) {
+            console.error('Error during logout:', error.message);
+        }
+    };
+
     return (
         <>
             <header className="site-header">
@@ -9,21 +18,21 @@ export default function Header() {
                     {/* not logged*/}
                     <Link className="header-btn" to="/about-nba">/ What is NBA</Link>
                     <Link className="header-btn" to="/teams">/ NBA Teams</Link>
-                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link>  
+                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link>
                     <Link className="header-btn" to="/login"> / Login</Link>
                     <Link className="header-btn" to="/register">/ Register</Link>
-                    
+
                     {/* logged user */}
                     <Link className="header-btn" to="/about-nba">/ What is NBA</Link>
                     <Link className="header-btn" to="/teams">/ NBA Teams</Link>
-                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link> 
+                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link>
 
                     {/* logged and admin */}
                     <Link className="header-btn" to="/create-card">/ Create Card</Link>
 
                     <Link className="header-btn" to="/profile">/ Profile</Link>
                     <Link className="header-btn" to="/cart">/ Cart</Link>
-                    <Link className="header-btn" to="/logout">/ Logout</Link>
+                    <Link className="header-btn" to="/logout" onClick={handleLogout}>/ Logout</Link>
 
                 </nav>
             </header>
