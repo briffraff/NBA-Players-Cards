45c17064f408d53cd5f414b2abb259699220c673
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-06 06:08:59 +0300

    update menu html and css (#8)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
 import { logoutUser } from "../../service/authentication/authService";
+import { useEffect, useState } from "react";
 
-export default function Header() {
+export default function Header(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        //check if user is logged in
+        const loggedIn = true;
+        setIsLoggedIn(loggedIn);
+    }, []);
+
     const handleLogout = async () => {
         try {
             await logoutUser();
+            setIsLoggedIn(false);
         } catch (error) {
             console.error('Error during logout:', error.message);
         }
@@ -13,28 +23,29 @@ export default function Header() {
     return (
         <>
             <header className="site-header">
-                <div className="logo">Logo</div>
-                <nav className="site-nav">
-                    {/* not logged*/}
-                    <Link className="header-btn" to="/about-nba">/ What is NBA</Link>
-                    <Link className="header-btn" to="/teams">/ NBA Teams</Link>
-                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link>
-                    <Link className="header-btn" to="/login"> / Login</Link>
-                    <Link className="header-btn" to="/register">/ Register</Link>
-
-                    {/* logged user */}
-                    <Link className="header-btn" to="/about-nba">/ What is NBA</Link>
-                    <Link className="header-btn" to="/teams">/ NBA Teams</Link>
-                    <Link className="header-btn" to="/cards-shop">/ Cards shop</Link>
-
-                    {/* logged and admin */}
-                    <Link className="header-btn" to="/create-card">/ Create Card</Link>
-
-                    <Link className="header-btn" to="/profile">/ Profile</Link>
-                    <Link className="header-btn" to="/cart">/ Cart</Link>
-                    <Link className="header-btn" to="/logout" onClick={handleLogout}>/ Logout</Link>
-
+                <img className="logo" src="../../public/assets/img/nba.png" alt="Logo" />
+                <nav className="main-menu">
+                    <Link className="header-btn" to="/about-nba">/ {props.menu[0]}</Link>
+                    <Link className="header-btn" to="/teams">/ {props.menu[1]}</Link>
+                    <Link className="header-btn" to="/cards-shop">/ {props.menu[2]}</Link>
                 </nav>
+                <div className="user-actions">
+                    {isLoggedIn ? (
+                        <div className="logged-in">
+                            <div className="greeting">
+                                <span className="hello">Hello /</span>
+                                <Link className="user-profile" to="/profile">User</Link>
+                            </div>
+                            <Link className="cart-btn" to="/cart">/ {props.menu[7]}</Link>
+                            <Link className="logout-btn" to="/logout" onClick={handleLogout}>/ {props.menu[8]}</Link>
+                        </div>
+                    ) : (
+                        <div className="logged-out">
+                            <Link className="header-btn" to="/login"> / {props.menu[3]}</Link>
+                            <Link className="header-btn" to="/register">/ {props.menu[4]}</Link>
+                        </div>
+                    )}
+                </div>
             </header>
         </>
     )
