47685304783f07d10db595b1fd9e1b5b7f02388e
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-07 17:09:47 +0300

    Add subscribers component; Error handling; update css (#13)

--- src/service/firebase/firestore/firestore-service.js
+++ src/service/firebase/firestore/firestore-service.js
@@ -1,8 +1,9 @@
 // teamsService.js
 import { db } from "../firebase-config";
-import { doc, getDoc, getDocs, collection } from "firebase/firestore";
+import { query, where, doc, getDoc, getDocs, collection, addDoc } from "firebase/firestore";
 
 const teamsCollectionRef = collection(db, "nba-teams");
+const subscriberCollectionRef = collection(db, "subscribers");
 
 export const getTeams = async () => {
     try {
@@ -30,6 +31,32 @@ export const getTeamById = async (id) => {
         }
     } catch (error) {
         console.error("Error fetching team: ", error);
+        throw error;
+    }
+};
+
+
+export const addSubscriber = async (email) => {
+    try {
+        const currentYear = new Date().getFullYear();
+
+        const q = query(subscriberCollectionRef, where("email", "==", email));
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+            let errorMessage = `Email ${email} already exists`
+            console.log(errorMessage);
+            throw new Error(errorMessage);
+        }
+
+        const newSubscriber = {
+            email: email,
+            subscribeOn: currentYear
+        };
+
+        await addDoc(subscriberCollectionRef, newSubscriber);
+        console.log(`Subscriber added: ${email}`);
+    } catch (error) {
         throw error;
     }
 };
