2963d026b1c4e04f7396fc4dfcc7be5b1b15989a
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-10 02:04:41 +0300

    update welcome login register (#20)

--- src/components/Teams/Teams.jsx
+++ src/components/Teams/Teams.jsx
@@ -48,7 +48,8 @@ export default function Teams() {
         return division
             .split('-')
             .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(' ');
+            .join(' ')
+            .trim();
     };
 
     return (
--- src/components/User/Logout/Logout.jsx
+++ src/components/User/Logout/Logout.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom"
 const logo = "https://firebasestorage.googleapis.com/v0/b/nba-player-cards.appspot.com/o/images%2Fcontent%2Fnba.png?alt=media&token=69987172-bd26-40c1-874a-fce01d97d9b8"
+
+
 export default function Logout() {
     return (
         <>
-            <section className="logout-box content">
-                <h1 className="welcome">Thank you for choosing logos.nba !</h1>
-                <h4 className="sub-head">Everything for</h4>
-                <Link to='/'><img className="nba" src={logo} alt="" /></Link>
-                <p className="sub-head">Teams</p>
-                <section className="log-reg">
-                    <p>You have no registration ?</p>
-                    <div>
-                        <Link className="login" to="/login">Login</Link>
-                        <p>|</p>
-                        <Link className="register" to="/register">register</Link>
-                    </div>
+            <div className="content-wrapper">
+                <section className="logout-box content">
+                    <h1 className="welcome">Thank you for visiting nbacards,</h1>
+                    <h1 className="welcome">See you next time! !</h1>
+                    <h4 className="sub-head">Everything for</h4>
+                    <Link to='/'><img className="nba" src={logo} alt="" /></Link>
+                    <p className="sub-head">Teams</p>
+                    <section className="log-reg">
+                        <p>You have no registration ?</p>
+                        <div>
+                            <Link className="login" to="/login">Login</Link>
+                            <p>|</p>
+                            <Link className="register" to="/register">register</Link>
+                        </div>
+                    </section>
                 </section>
-            </section>
+            </div>
         </>
     )
 }
