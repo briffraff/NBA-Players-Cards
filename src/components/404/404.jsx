2835ae3e35e9ff813ff3ed6ad96fe0dbe97a49a7
Borislav Borisov <briffraff6@gmail.com>
2024-07-13 17:40:34 +0300

    update 404 css

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -5,11 +5,13 @@ export default function NotFound() {
     return (
         <>
             <div className={styles.notFound}>
-                <h1>404</h1>
+                <h1 className={styles.error}>
+                    !<h1 className={styles.errorNumber}>404</h1>
+                </h1>
                 <h2>Page Not Found</h2>
                 <h4>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h4>
                 <Link to="/" className={styles.btn}>Go to Home</Link>
-            </div>
+            </div >
         </>
     )
 }
