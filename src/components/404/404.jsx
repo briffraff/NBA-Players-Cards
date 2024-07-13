84a1b455837cc92efc8efdac7323c75e00517627
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-13 18:40:25 +0300

    update 404 component (#32)

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 import styles from "../../../public/assets/css/modules/NotFound404.module.css"
-
+import rim from "../../../public/assets/img/readme/rim.svg"
 export default function NotFound() {
     return (
         <>
-            <div className={styles.notFound}>
-                <h1 className={styles.error}>
-                    !<h1 className={styles.errorNumber}>404</h1>
-                </h1>
-                <h2>Page Not Found</h2>
-                <h4>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h4>
-                <Link to="/" className={styles.btn}>Go to Home</Link>
-            </div >
+            <div className={styles.container}>
+                <img src={rim} alt="Rim" width={300} height={300} />
+                <div className={styles.notFound}>
+                    <h1 className={styles.error}>
+                        !<h1 className={styles.errorNumber}>404</h1>
+                    </h1>
+                    <h2>Page Not Found</h2>
+                    <h4>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h4>
+                    <Link to="/" className={styles.btn}>Go to Home</Link>
+                </div >
+            </div>
         </>
     )
 }
