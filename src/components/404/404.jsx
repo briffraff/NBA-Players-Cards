e0c258419d2e6d7b85a289b1a8157130cf14f4e6
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-13 17:18:30 +0300

    add css module to 404 component; config vite camelCase import (#30)

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../../../public/assets/css/modules/NotFound404.module.css"
 
 export default function NotFound() {
     return (
         <>
-            <div style={{ textAlign: "center", marginTop: "50px" }}>
-                <h1>404 - Page Not Found</h1>
-                <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
-                <Link to="/">Go to Home</Link>
+            <div className={styles.notFound}>
+                <h1>404</h1>
+                <h2>Page Not Found</h2>
+                <h4>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h4>
+                <Link to="/" className={styles.btn}>Go to Home</Link>
             </div>
         </>
     )
