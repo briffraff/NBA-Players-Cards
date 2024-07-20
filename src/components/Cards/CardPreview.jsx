aad6e89fe08e32f875ce12202709fb2714ff80cd
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-07-20 04:50:57 +0300

    add creating card (#52)

---
+++ src/components/Cards/CardPreview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../../../public/assets/css/modules/_CreateCard.module.scss';
+
+export default function CardPreview({ formData }) {
+    return (
+        <div className={styles.cardPreviewContainer}>
+            <div className={styles.cardHolder}>
+                <img className={styles.cardImage} src={formData.urls.Front} alt="Front" />
+                <h2 className={styles.playerName}>{formData.playerName}</h2>
+                <p className={styles.shortInfo}>{formData.shortInfo}</p>
+            </div>
+        </div>
+    );
+}
