2ebf9ffced8ead7378f4777772c5782cddc36923
Borislav Borisov <50926510+briffraff@users.noreply.github.com>
2024-08-01 14:21:10 +0300

    Search bar (#68)

---
+++ src/components/Cards/SearchBar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styles from "../../../public/assets/scss/modules/_SearchBar.module.scss";
+import { useCards } from "../../contexts/cardsContext";
+
+export default function SearchBar() {
+    const [searchFor, setSearchFor] = useState("");
+    const { searchCard } = useCards();
+    const [foundedCard, setFoundedCard] = useState(null);
+    const [error, setError] = useState(null);
+
+    const handleSearch = async () => {
+        try {
+            setError(null); 
+            await searchCard(searchFor);
+        } catch (error) {
+            setError("Cannot find a card with that name.");
+        }
+    };
+
+    return (
+        <section className={styles.searchBar}>
+            <div className={styles.container}>
+                <input
+                    name="searchInput"
+                    id="searchInput"
+                    value={searchFor}
+                    className={styles.searchInput}
+                    type="text"
+                    placeholder="Search for player card"
+                    onChange={(e) => setSearchFor(e.target.value)}
+                />
+                <button onClick={handleSearch} className={styles.searchBtn}>
+                    Search
+                </button>
+            </div>
+            {error && <div className={styles.notFound}>{error}</div>}
+        </section>
+    );
+}
--- src/contexts/cardsContext.jsx
+++ src/contexts/cardsContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
-import { loadCards, getAllRemainCards, loadMore, getTotalCardsCount } from "../service/firebase/firestore/firestore-service";
+import { loadCards, getAllRemainCards, loadMore, getTotalCardsCount, getCardByPlayerName } from "../service/firebase/firestore/firestore-service";
 const CardsContext = createContext();
 
 export function useCards() {
@@ -12,6 +12,7 @@ export default function CardsProvider({ children }) {
     const [loading, setLoading] = useState(false);
     const [totalCardsCount, setTotalCardsCount] = useState(0);
     const [currentCardsCount, setCurrentCardsCount] = useState(0);
+    const [foundedCard, setFoundedCard] = useState([]);
 
     useEffect(() => {
         const fetchTotalCardsCount = async () => {
@@ -77,14 +78,31 @@ export default function CardsProvider({ children }) {
         }
     };
 
+    const searchCard = async (searchForPlayer) => {
+        setLoading(true);
+
+        try {
+            const result = await getCardByPlayerName(searchForPlayer);
+            console.log(result);
+            setFoundedCard(result);
+            setLoading(false);
+        } catch (error) {
+            setFoundedCard(null);
+            setLoading(false);
+            throw error;
+        }
+    };
+
     return (
         <CardsContext.Provider value={{
             cards,
+            foundedCard,
             loadMoreCards,
             loadAll,
             loading,
             totalCardsCount,
-            currentCardsCount
+            currentCardsCount,
+            searchCard,
         }}>
             {children}
         </CardsContext.Provider>
